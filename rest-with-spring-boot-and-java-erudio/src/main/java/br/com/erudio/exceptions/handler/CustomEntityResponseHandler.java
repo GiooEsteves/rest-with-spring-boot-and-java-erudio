15e755d4c1d9438d3216c62847fb1f16868d0ef3
Giovanna Esteves <gio_esteves@hotmail.com>
2025-04-02 09:59:28 -0300

    setting repositories

--- rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/exceptions/handler/CustomEntityResponseHandler.java
+++ rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/exceptions/handler/CustomEntityResponseHandler.java
@@ -1,7 +1,7 @@
 package br.com.erudio.exceptions.handler;
 
 import br.com.erudio.exceptions.ExceptionResponse;
-import br.com.erudio.exceptions.UnsupportedMathOperationsException;
+import br.com.erudio.exceptions.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -21,9 +21,9 @@ public class CustomEntityResponseHandler extends ResponseEntityExceptionHandler 
         return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
-    @ExceptionHandler(UnsupportedMathOperationsException.class)
-    public final ResponseEntity<ExceptionResponse> handleBadRequestException(Exception e, WebRequest request){
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public final ResponseEntity<ExceptionResponse> handleNotFoundException(Exception e, WebRequest request){
         ExceptionResponse response = new ExceptionResponse(new Date(), e.getMessage(), request.getDescription(false));
-        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
     }
 }
